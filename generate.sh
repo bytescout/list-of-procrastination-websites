e1e18129b56807c2d21a9154db7c7abcd883de73
Eugene Mi <eugene.mironichev@gmail.com>
2020-10-27 23:09:52 +0300

    more lists added and updated, minor scripts changes

--- generate.sh
+++ generate.sh
@@ -1,28 +1,31 @@
+# this script was tested on Ubuntu on Windows
+
+echo "Combining all lists into one"
 cat ./lists/*.txt > sites-all.txt
+echo "done"
 
 cp sites-all.txt temp1.txt
 
-# clean from empty lines
+echo "cleaning from empty lines"
 sed '/^$/d' temp1.txt > temp2.txt
 
-# now sort and remove duplicated lines
+echo "now sorting and removing duplicated websites"
 sort temp2.txt | uniq > temp3.txt
 
-# removing www. from lists (if any) and then add copy of every website with www.
+echo "removing www. from lists (if any) and then add copy of every website with www."
 sed 's/^www\.\(.*\)$/\1/' temp3.txt > temp4.txt
 
-# now add www. variation for every website
+echo "now add www. variation for every website"
 sed 's/^\(.*\)$/\1\r\nwww\.\1/' temp4.txt > temp5.txt
 
-
-# updating sites-all.txt with clean and sorted version
+echo "updating sites-all.txt with clean and sorted version"
 rm sites-all.txt
 cp temp5.txt sites-all.txt
 
-# generate hosts file for Windows
+echo "generating hosts.txt file for Windows"
 rm hosts.txt
 sed 's/^\(.*\)$/127.0.0.1 \1/' sites-all.txt > hosts.txt
+sed -i '1 i\# copy the content of this file and paste at the the end of "c:\\windows\\system\\drivers\\etc\\hosts" file' hosts.txt
 
-# cleanup
-
+echo "final cleanup for temp files"
 rm temp1.txt temp2.txt temp3.txt temp4.txt temp5.txt
